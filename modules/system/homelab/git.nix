3c1fa2410e00d089967f57ced2c6f08ce4453cd2
Satria <admin@satr14.my.id>
2026-03-02 15:04:55 +0700

    enable git oidc

--- modules/system/homelab/git.nix
+++ modules/system/homelab/git.nix
@@ -8,19 +8,19 @@
         HTTP_ADDR = "127.0.0.1";
         HTTP_PORT = 5080;
         PROTOCOL = "http";
-        ROOT_URL = "https://git.${homelab.domain}";
+        ROOT_URL = "https://git.proxy.${homelab.domain}";
         LANDING_PAGE = "explore";
       };
       oauth2_client.ENABLE_AUTO_REGISTRATION=true;
       service = {
-        # DISABLE_REGISTRATION = true;
-        # ENABLE_OPENID_SIGNIN = true;
-        # ENABLE_OPENID_SIGNUP = true;
-        # ENABLE_INTERNAL_SIGNIN = false;
-        # SHOW_REGISTRATION_BUTTON = false;
-        # ALLOW_ONLY_EXTERNAL_REGISTRATION = true;
-        # ALLOW_ONLY_INTERNAL_REGISTRATION = false;
-        # REQUIRE_EXTERNAL_REGISTRATION_PASSWORD = true;
+        DISABLE_REGISTRATION = true;
+        ENABLE_OPENID_SIGNIN = false;
+        ENABLE_OPENID_SIGNUP = false;
+        ENABLE_INTERNAL_SIGNIN = false;
+        SHOW_REGISTRATION_BUTTON = false;
+        ALLOW_ONLY_EXTERNAL_REGISTRATION = true;
+        ALLOW_ONLY_INTERNAL_REGISTRATION = false;
+        REQUIRE_EXTERNAL_REGISTRATION_PASSWORD = true;
       };
       ui = {
         # THEMES = "catppuccin-blue-auto,catppuccin-mocha-blue,catppuccin-sapphire-auto,catppuccin-mocha-sapphire,auto";
