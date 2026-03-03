5a689decf375ece202c70329ecab25dc3d668505
Satria <admin@satr14.my.id>
2026-03-03 21:46:23 +0700

    fix openwebui auth

--- modules/system/homelab/proxy.nix
+++ modules/system/homelab/proxy.nix
@@ -53,14 +53,20 @@ in {
         proxyWebsockets = true;
         basicAuthFile = if cfg.auth then "/var/lib/nginx/.htpasswd" else null;
         extraConfig = ''
-          proxy_set_header X-Auth-User $remote_user;
+          proxy_http_version 1.1;
+          proxy_set_header Upgrade $http_upgrade;
+          proxy_set_header Connection "upgrade";
+          # proxy_set_header X-Auth-User $remote_user;
+          proxy_set_header Authorization "";
           proxy_set_header X-Real-IP $remote_addr;
           proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
           proxy_set_header X-Forwarded-Proto $scheme;
+          proxy_read_timeout 600s;
+          proxy_send_timeout 600s;
+          proxy_buffering off;
+          proxy_cache off;
+          send_timeout 600s;
           client_max_body_size 50000M;
-          proxy_read_timeout   600s;
-          proxy_send_timeout   600s;
-          send_timeout         600s;
         '';
       };
     }) proxy-mappings;
