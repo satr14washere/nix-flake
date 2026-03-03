45c5ed21fe14e56e2b36c688934ebd3c3ffe8a29
Satria <admin@satr14.my.id>
2026-03-03 17:55:05 +0700

    fix ixon

--- modules/system/homelab/dash.nix
+++ modules/system/homelab/dash.nix
@@ -59,7 +59,7 @@
     [ "Forgejo" "forgejo" "https://git.proxy.${homelab.domain}" "http://localhost:5080/" ]
     [ "Dockge" "docker" "https://containers.proxy.${homelab.domain}" "http://localhost:5001/" ]
     [ "Ollama" "ollama" "https://ai.proxy.${homelab.domain}" "http://localhost:8080/" ]
-    [ "Guacamole" "guacamole" "https://remote.proxy.${homelab.domain}" "http://localhost:8085/" ]
+    [ "Guacamole" "apacheguacamole" "https://remote.proxy.${homelab.domain}" "http://localhost:8085/" ]
     [ "Immich" "immich" "https://gallery.proxy.${homelab.domain}" "http://localhost:2283/" ]
   ];
   bookmarks = [
