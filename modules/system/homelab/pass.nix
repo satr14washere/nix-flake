d8857f46f97da17ccc6e66d4d85cfc990ff8a570
Satria <admin@satr14.my.id>
2026-03-06 14:44:28 +0700

    update config vaultwarden

--- modules/system/homelab/pass.nix
+++ modules/system/homelab/pass.nix
@@ -2,11 +2,12 @@
   services.vaultwarden = {
     enable = true;
     config = {
-      DOMAIN = "pass.proxy.${homelab.domain}";
-      SIGNUPS_ALLOWED = true;
-      ROCKET_ADDRESS = "127.0.0.1";
+      DOMAIN = "https://pass.proxy.${homelab.domain}";
       ROCKET_PORT = 8060;
+      ROCKET_ADDRESS = "127.0.0.1";
       ROCKET_LOG = "critical";
+      DATA_FOLDER = "/mnt/data/vaultwarden";
+      SIGNUPS_ALLOWED = true;
     };
   };
 }
