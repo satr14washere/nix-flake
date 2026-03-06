e732fcdcaee067984e5ad82456d463ee615a6941
Satria <admin@satr14.my.id>
2026-03-06 22:34:38 +0700

    reenable auth

--- modules/system/homelab/ai.nix
+++ modules/system/homelab/ai.nix
@@ -6,15 +6,17 @@
       port = 11434;
       user = "ollama";
       home = "/mnt/data/ollama";
-      # loadModels = [ "gemma3n:e4b" "gemma3n:e2b"  ];
+      loadModels = [ 
+        "gemma3n:e4b" # "gemma3n:e2b" 
+        # "codellama:7b" "starcoder:3b"
+      ];
     };
     open-webui = {
       enable = true;
       port = 8080;
       environment = {
         OLLAMA_BASE_URL = "http://localhost:11434";
-        WEBUI_AUTH = "False";
-        
+        # WEBUI_AUTH = "False";
       };
     };
   };
