6ca1d51cba2089387e6353b1351590a0a634a985
Satria <admin@satr14.my.id>
2026-03-12 22:01:23 +0700

    add usermap

--- modules/system/homelab/remote.nix
+++ modules/system/homelab/remote.nix
@@ -8,6 +8,7 @@
     guacamole-client = {
       enable = true;
       enableWebserver = true;
+      userMappingXml = "/mnt/data/guacamole/user-mapping.xml";
       settings = {
         guacd-hostname = "127.0.0.1";
         guacd-port = 4822;
