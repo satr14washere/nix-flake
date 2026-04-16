99c5fade9bbafafb3b0b0b8184b306f06931f1d3
satr14 <admin@satr14.my.id>
2026-04-16 21:37:26 +0700

    revert because of error

--- modules/system/homelab/pass.nix
+++ modules/system/homelab/pass.nix
@@ -5,7 +5,7 @@
     backupDir = "/mnt/data/apps/vaultwarden/backups";
     environmentFile = "/mnt/data/apps/vaultwarden/.env";
     config = {
-      DATA_FOLDER = "/mnt/data/apps/vaultwarden/data";
+      # DATA_FOLDER = "/mnt/data/apps/vaultwarden/data"; # [vaultwarden][ERROR] Error creating private key '/mnt/data/apps/vaultwarden/data/rsa_key.pem'
       ROCKET_PORT = 8060;
       ROCKET_ADDRESS = "127.0.0.1";
       ROCKET_LOG = "critical";
