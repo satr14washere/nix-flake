bd7e1766714f4c25a34a1a121ba1ef0b8887840b
satr14 <admin@satr14.my.id>
2026-04-29 19:00:06 +0700

    cryptpad config

--- modules/system/homelab/docs.nix
+++ modules/system/homelab/docs.nix
@@ -1,15 +1,30 @@
 { homelab, ... }: let
   domain = "docs.${homelab.domain}";
+  sandbox = "docs-sandbox.${homelab.domain}";
+  data-dir = "/mnt/data/apps/cryptpad";
 in {
   services.cryptpad = {
     enable = true;
     settings = {
       httpPort = 7090;
-      websocketPort = 7080;
+      httpAddress = "127.0.0.1";
       httpUnsafeOrigin = "https://${domain}";
-      httpSafeOrigin = "https://${domain}";
+      httpSafeOrigin = "https://${sandbox}";
       blockDailyCheck = true;
       disableIntegratedEviction = true;
+      archivePath = "${data-dir}/archive";
+      pinPath = "${data-dir}/pins";
+      taskPath = "${data-dir}/tasks";
+      blockPath = "${data-dir}/block";
+      blobPath = "${data-dir}/blob";
+      blobStagingPath = "${data-dir}/blobstage";
+      decreePath = "${data-dir}/decrees";
+      logPath = "${data-dir}/logs";
     };
+  };
+  
+  systemd.services.cryptpad.serviceConfig = {
+    ReadWritePaths = [ data-dir ];
+    ProtectMountPoints = false;
   };
 }
