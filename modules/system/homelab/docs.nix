b135e301f1b2baceb9700048e1ced54bc1e66401
satr14 <admin@satr14.my.id>
2026-05-01 08:18:29 +0700

    fix cryptpad websocket port

--- modules/system/homelab/docs.nix
+++ modules/system/homelab/docs.nix
@@ -1,11 +1,11 @@
 { homelab, ... }: let
   domain = "docs.${homelab.domain}";
   sandbox = "docs-sandbox.${homelab.domain}";
-  data-dir = "/mnt/data/apps/cryptpad";
 in {
   services.cryptpad = {
     enable = true;
     settings = {
+      websocketPort = 7091;
       httpPort = 7090;
       httpAddress = "127.0.0.1";
       httpUnsafeOrigin = "https://${domain}";
