c3db68fad698a12efb507bbf5ef3189bcb15ef5c
satr14 <admin@satr14.my.id>
2026-05-14 09:21:54 +0700

    try powersave

--- modules/system/homelab/mc.nix
+++ modules/system/homelab/mc.nix
@@ -12,7 +12,7 @@ in {
   imports = [ inputs.mc.nixosModules.minecraft-servers ];
   nixpkgs.overlays = [ inputs.mc.overlay ];
   
-  powerManagement.cpuFreqGovernor = "performance";
+  powerManagement.cpuFreqGovernor = "powersave";
   boot.kernel.sysctl = {
     "vm.nr_hugepages" = (ram-allocation-mb / 2) + 512; # (heap_mb / 2MB per page) + 512 pages (1GB) for ZGC off-heap overhead
     "vm.swappiness" = 10;
