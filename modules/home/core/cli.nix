6a106d606c28cdffe6e0d82aed9f1b93a25776b9
satr14 <admin@satr14.my.id>
2026-05-17 07:23:54 +0700

    add clipboard support

--- modules/home/core/cli.nix
+++ modules/home/core/cli.nix
@@ -49,10 +49,20 @@
       initLua = ''
         vim.opt.clipboard = "unnamedplus"
         vim.opt.termguicolors = true
+        vim.g.clipboard = {
+          name = "OSC 52",
+          copy = {
+            ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+            ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+          },
+          paste = {
+            ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+            ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+          },
+        }
         require("nvim-tree").setup()
         vim.api.nvim_create_autocmd("VimEnter", {
           callback = function()
-            -- vim.cmd("NvimTreeOpen")
             vim.cmd("set nu")
             vim.cmd.wincmd 'p'
           end,
