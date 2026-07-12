fe1820fdb88b6cb4a02d2c7402f2b735790ed43b
satr14 <admin@satr14.my.id>
2026-07-12 12:16:39 +0700

    fix typo

--- modules/home/rice/keybinds.nix
+++ modules/home/rice/keybinds.nix
@@ -54,7 +54,7 @@
     "Mod+V".action.spawn = [ "rofi" "-modi" "clipboard:cliphist-rofi-img" "-show" "clipboard" "-show-icons" ];
     "Mod+A".action.spawn = [ "zeditor" ];
     "Mod+C".action.spawn = [ "kitty" "btop" ];
-    "Mod+Shift+F".action.spawn-sh = "${lib.getExe pkgs.labwc} $$ WAYLAND_DISPLAY=wayland-1 ${lib.getExe pkgs.kitty}";
+    "Mod+Shift+F".action.spawn-sh = "${lib.getExe pkgs.labwc} & WAYLAND_DISPLAY=wayland-1 ${lib.getExe pkgs.kitty}";
     "Mod+Shift+C".action.spawn = [ "kitty" "zsh" "-c" "fastfetch; exec zsh -i" ];
     "Mod+Shift+D".action.spawn = [ "steam" "-system-composer" "steam://open/bigpicture" ];
     "Mod+D".action.spawn = [ "prismlauncher" ];
