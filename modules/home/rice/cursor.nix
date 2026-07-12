309ef2ecfe61a55f93f0c7bb6ac0d3dbc5aa6e9f
satr14 <admin@satr14.my.id>
2026-07-12 12:14:08 +0700

    cursor fix

--- modules/home/rice/cursor.nix
+++ modules/home/rice/cursor.nix
@@ -2,6 +2,7 @@
   catppuccin.cursors.enable = false; # managed manually below to use the "Light" cursor variant
 
   home.pointerCursor = {
+    enable = true;
     gtk.enable = true;
     package = pkgs.catppuccin-cursors."${ctp-opt.flavor}Light";
     name = lib.mkOverride 30 "catppuccin-${ctp-opt.flavor}-light-cursors";
