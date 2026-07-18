15c912d324834af5e0261c68195b0978ee8f8000
satr14 <admin@satr14.my.id>
2026-07-19 06:26:33 +0700

    cdn thumbnail support

--- modules/system/homelab/cdn.nix
+++ modules/system/homelab/cdn.nix
@@ -1,18 +1,21 @@
 { pkgs, ... }: let
-  version = "v1.20.18";
-  executable = pkgs.fetchurl {
+  python = pkgs.python3.withPackages (ps: [ ps.pillow ]);
+  script = let
+    version = "v1.20.18";
+  in pkgs.fetchurl {
     url = "https://github.com/9001/copyparty/releases/download/${version}/copyparty-en.py";
     hash = "sha256-8SBrKaLPat80n8sONKQYFeFSQXUnCYtwcOU7SR52h7E=";
-  }; 
+  };
 in {
   systemd.services.copyparty = {
     description = "File Sharing Service";
     enable = true;
     after = [ "network.target" ];
     wantedBy = [ "multi-user.target" ];
+    path = [ pkgs.ffmpeg ];
     serviceConfig = {
       # ExecStart = "${pkgs.copyparty-most}/bin/copyparty -c /mnt/share/cfg/files.conf";
-      ExecStart = "${pkgs.python3}/bin/python3 ${executable} -c /mnt/share/cfg/files.conf";
+      ExecStart = "${python}/bin/python3 ${script} -c /mnt/share/cfg/files.conf";
       Restart = "on-failure";
     };
   };
