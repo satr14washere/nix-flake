30f3ef32fadbbab480012615e157d6ec428c8965
satr14 <admin@satr14.my.id>
2026-07-20 18:55:44 +0700

    fix origins

--- modules/system/homelab/control.nix
+++ modules/system/homelab/control.nix
@@ -1,7 +1,7 @@
 { homelab, pkgs, ... }: {
   services.cockpit = {
     enable = true;
-    allowed-origins = [ "control.${homelab.proxy.base}" ];
+    allowed-origins = [ "https://control.proxy.${homelab.domain}" ];
     plugins = with pkgs; [
       cockpit-files cockpit-machines
     ];
