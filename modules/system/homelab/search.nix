f3c7afabfb39cc61043843574531c1bfc24576e3
satr14 <admin@satr14.my.id>
2026-08-28 22:58:01 +0700

    openwebui specifics

--- modules/system/homelab/search.nix
+++ modules/system/homelab/search.nix
@@ -16,6 +16,14 @@
         privacy_policy_url = false;
         enable_metrics = true;
       };
+      search.formats = [ "html" "json" ];
+    };
+    limiterSettings = {
+      botdetection.ip_limit.link_token = false;
+      botdetection.ip_lists = {
+        block_ip = [ ];
+        pass_ip = [ ];
+      };
     };
   };
 }
