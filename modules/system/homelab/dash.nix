79d2e02e0a2681388899dba5528b3c0b2fcf8439
satr14 <admin@satr14.my.id>
2026-09-16 20:16:01 +0700

    less distracting dashboard

--- modules/system/homelab/dash.nix
+++ modules/system/homelab/dash.nix
@@ -1,25 +1,4 @@
 { timezone, homelab, ... }: let 
-  yt = [
-    "UCXuqSBlHAE6Xw-yeJA0Tunw" # LinusTechTips
-    "UCsBjURrPoezykLs9EqgamOA" # Fireship
-    "UC2Xd-TjJByJyK2w1zNwY0zQ" # BeyondFireship
-    "UC6biysICWOJ-C3P4Tyeggzg" # LowLevel
-    "UCR-DXc1voovS8nhAvccRZhg" # JeffGeerling
-    "UCzgA9CBrIXPtkB2yNTTiy1w" # Level2Jeff
-    "UCgdTVe88YVSrOZ9qKumhULQ" # HardwareHaven
-    "UCOk-gHyjcWZNj3Br4oxwh0A" # TechnoTim
-    "UCZNhwA1B5YqiY1nLzmM0ZRg" # ChristianLempa
-    "UC9evhW4JB_UdXSLeZGy8lGw" # RaidOwl
-    "UCHnyfMqiRRG1u-2MsSQLbXA" # Veritasium
-    "UCYO_jab_esuFRV4b17AJtAw" # 3Blue1Brown
-    "UChIs72whgZI9w6d6FhwGGHA" # GamersNexus
-    "UCY1kMZp36IQSyNx_9h4mpCg" # MarkRober
-    "UC8ENHE5xdFSwx71u3fDH5Xw" # ThePrimeagen
-    "UCUyeluBRhGPCW4rPe_UvBZQ" # ThePrimeTime
-    "UCv6J_jJa8GJqFwQNgNrMuww" # ServeTheHome
-    "UCGKEMK3s-ZPbjVOIuAV8clQ" # CoreDumped
-    "UCWQaM7SpSECp9FELz-cHzuQ" # DreamsOfCode
-  ];
   monitor = [
     [ "DNS" "http://localhost:8088/" ]
     [ "Proxy" "https://proxy.${homelab.domain}/" ]
@@ -66,6 +45,17 @@ in {
               size = "small";
               widgets = [
                 {
+                  type = "clock";
+                  hide-header = true;
+                  hour-format = "12h";
+                  timezone = timezone;
+                }
+                {
+                  type = "calendar";
+                  hide-header = true;
+                  first-day-of-week = "monday";
+                }
+                {
                   type = "monitor";
                   title = "Critical Systems";
                   cache = "15s";
@@ -107,15 +97,15 @@ in {
                     }
                   ];
                 }
-                {
-                  type = "to-do";
-                  id = "tasks";
-                }
               ];
             }
             {
               size = "full";
               widgets = [
+                {
+                  type = "to-do";
+                  id = "tasks";
+                }
                 {
                   type = "server-stats";
                   servers = [{
@@ -150,46 +140,6 @@ in {
                 }
               ];
             }
-          ];
-        }
-        {
-          name = "Home";
-          show-mobile-header = true;
-          columns = [
-            {
-              size = "small";
-              widgets = [
-                {
-                  type = "clock";
-                  hide-header = true;
-                  hour-format = "12h";
-                  timezone = timezone;
-                }
-                {
-                  type = "calendar";
-                  hide-header = true;
-                  first-day-of-week = "monday";
-                }
-              ];
-            }
-            {
-              size = "full";
-              widgets = [
-                {
-                  type = "hacker-news";
-                  hide-header = true;
-                  limit = 5;
-                  cache = "1h";
-                }
-                {
-                  type = "videos";
-                  hide-header = true;
-                  style = "grid-cards";
-                  limit = 18;
-                  channels = yt;
-                }
-              ];
-            }
             {
               size = "small";
               widgets = [
@@ -215,6 +165,12 @@ in {
                   issues-limit = 3;
                   commits-limit = 0;
                   hide-header = true;
+                }
+                {
+                  type = "hacker-news";
+                  hide-header = true;
+                  limit = 5;
+                  cache = "1h";
                 }
               ];
             }
